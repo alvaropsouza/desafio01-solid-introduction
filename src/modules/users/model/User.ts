395525fe03c4a2789104df5d0636c12d453e2e6f
alvaropsouza <alvaropatricio61@gmail.com>
2021-10-11 21:46:44 -0300

    fix: correção nos métodos da usersRepository

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -1,7 +1,22 @@
-import { v4 as uuidV4 } from "uuid";
+import { v4 as uuidv4 } from "uuid";
 
 class User {
-  // Complete aqui
+  constructor() {
+    if (!this.id) {
+      this.id = uuidv4();
+    }
+
+    if (!this.admin) {
+      this.admin = false;
+    }
+  }
+
+  id?: string;
+  name: string;
+  admin?: boolean;
+  email: string;
+  created_at: Date;
+  updated_at: Date;
 }
 
 export { User };
