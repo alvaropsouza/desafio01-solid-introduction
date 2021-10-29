9750da4a9a00dbd445d361b38ff9f8ecdaa0fec2
alvaropsouza <alvaropatricio61@gmail.com>
2021-10-29 10:39:51 -0300

    Fix: Métodos com problemas, faltando apenas 4 erros

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -1,15 +1,21 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express'
 
-import { TurnUserAdminUseCase } from "./TurnUserAdminUseCase";
+import { TurnUserAdminUseCase } from './TurnUserAdminUseCase'
 
 class TurnUserAdminController {
   constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    const { id } = request.body;
+    const { user_id } = request.body;
+    
+    try {
+      const user = this.turnUserAdminUseCase.execute({ user_id })
 
-    this.turnUserAdminUseCase.execute({ id });
+      return response.send(user)
+    } catch (error) {
+        return response.status(404).send({ error: error })
+    }
   }
 }
 
-export { TurnUserAdminController };
+export { TurnUserAdminController }
